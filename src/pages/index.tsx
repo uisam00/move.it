4e522abb3c172008d26ae60e2047743bd70467ef
Ui Sam dos Santos <uisam.santos@gmail.com>
2021-03-10 13:35:44 -0300

    update name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home(props: HomeProps) {
     >
       <div className={styles.container} >
         <Head>
-          <title>Inicio | Mexa.se</title>
+          <title>Inicio | Move.it</title>
         </Head>
         <ExperienceBar />
         
